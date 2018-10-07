061fa362db78ea2b9880d8c838b19d9d7541e1c7
Yassir Awad <yassir.awad@dce.sa>
2018-10-07 14:22:39 +0300

    first commit

---
+++ src/Postgis.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Digitalcloud\Postgis;
+
+use Illuminate\Database\Eloquent\Builder;
+use Phaza\LaravelPostgis\Eloquent\PostgisTrait;
+use Phaza\LaravelPostgis\Geometries\Point;
+
+trait Postgis
+{
+    use PostgisTrait;
+
+    /**
+     * @param Builder $query
+     * @param Point $location
+     * @return Builder
+     */
+    public function scopeWithDistance(Builder $query, Point $location)
+    {
+        $classQuerry = $query->getQuery();
+
+        if ($classQuerry && !$classQuerry->columns) {
+            $query->select([$classQuerry->from . '.*']);
+        }
+
+        $division = 1;
+
+        if (property_exists(static::class, 'unit') && static::$unit == "mile") {
+            $division = 0.000621371;
+        } elseif (property_exists(static::class, 'unit') && static::$unit == "km") {
+            $division = 1000;
+        }
+
+        $q = "ST_Distance({$this->getLocationColumn()},ST_GeomFromText('POINT({$location->getLng()} {$location->getLat()})',4326))/{$division}";
+
+        return $query->selectSub($q, 'distance');
+    }
+
+    /**
+     * @param Builder $query
+     * @param Point $location
+     * @param float $inner_radius
+     * @param float $outer_radius
+     * @return Builder
+     */
+    public function scopeWithGeofence(Builder $query, Point $location, $inner_radius, $outer_radius)
+    {
+        $query = $this->scopeWithDistance($query, $location);
+
+        return $query->havingRaw("distance BETWEEN {$inner_radius} AND {$outer_radius}");
+    }
+
+    /**
+     * @param Builder $query
+     * @param Point $location
+     * @param float $inner_radius
+     * @param float $outer_radius
+     * @return Builder
+     */
+    public function scopeWhereDistance(Builder $query, Point $location, $operator, $units)
+    {
+        $classQuerry = $query->getQuery();
+
+        if ($classQuerry && !$classQuerry->columns) {
+            $query->select([$classQuerry->from . '.*']);
+        }
+
+        $q = "ST_Distance({$this->getLocationColumn()},ST_GeomFromText('POINT({$location->getLng()} {$location->getLat()})',4326))";
+
+        return $query->whereRaw("$q {$operator} {$units}");
+    }
+
+
+    public function getLocationColumn()
+    {
+        $column = defined('static::LOCATION') ? static::LATITUDE : 'location';
+
+        return $this->getTable() . '.' . $column;
+    }
+}
